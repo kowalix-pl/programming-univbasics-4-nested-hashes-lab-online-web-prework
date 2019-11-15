7f1edd8be97046f07cbc2a90ae0b625d889093a5
kowalix007 <kowalix007@gmail.com>
2019-11-15 16:11:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -121,5 +121,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
- 
+ programmer_hash = {dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["Assembly","C"]}}
 end
